a8e84530e437da2b3b40ba7b7ebad2806b55c643
“Tyler <tylerpalef@gmail.com>
2018-08-28 11:44:32 -0400

    finished portion 3 regarding establishing the rider test

--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -7,6 +7,10 @@ class RiderTest < ActiveSupport::TestCase
     # this time use create( ... ) instead of build
     aegon = nil
     daenerys = nil
+    aegon = create(:rider, name: "Aegon", sword: "Blackfyre")
+    daenerys = create(:rider, name: "Daenerys", sword: "Lightbringer")
+
+    binding.pry
 
     # can't change the assert_ statements!
     skip unless aegon && daenerys
